e1fb8a88688332194a49aabbf0b555771f97a745
NbOpposite <admin@opposite.biz>
2018-08-05 16:15:00 +0200

    Fixed location of encrypted key in travis deploy script

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -42,7 +42,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $encrypted_d9d8d4fbc4cd_key -iv $encrypted_d9d8d4fbc4cd_iv -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $encrypted_d9d8d4fbc4cd_key -iv $encrypted_d9d8d4fbc4cd_iv -in travis/deploy_key.enc -out deploy_key -d
 chmod 600 deploy-key
 eval `ssh-agent -s`
 ssh-add deploy-key
