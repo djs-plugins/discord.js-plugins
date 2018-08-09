b5364feabda656be05214e357bfc7d280a56ccbe
Travis CI <admin@opposite.biz>
2018-08-09 06:09:22 +0000

    Site build: f33ee276ecdb67ad6c6a4aa928d6faa4e848ba54

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const GITHUB_ORG = 'https://raw.githubusercontent.com/NbOpposite/';
+const GITHUB_ORG = 'https://raw.githubusercontent.com/djs-plugins/';
 
 self.addEventListener('fetch', event => {
   const req = event.request;
